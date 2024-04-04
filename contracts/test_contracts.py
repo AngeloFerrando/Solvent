cda7d733e6688cb48002081fbe2a2a2b80e3b00e
AngeloFerrando <angelo.ferrando42@gmail.com>
2024-04-04 14:23:29 +0200

    test updated

--- contracts/test_contracts.py
+++ contracts/test_contracts.py
@@ -3,6 +3,7 @@ import subprocess
 import pytest
 
 N_Transactions = 5
+Timeout = 10
 
 def run_makefile(folder):
     os.chdir(folder)
@@ -10,34 +11,37 @@ def run_makefile(folder):
     sol_files = [file for file in files if file.endswith('.sol')]
 
     for sol in sol_files:
-        compile_process = subprocess.Popen(['make', 'compile', f'Contract={sol}', f'N_Transactions={N_Transactions}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        compile_output, compile_error = compile_process.communicate()
+        try:
+            # Compile the code
+            compile_process = subprocess.run(['make', 'compile', f'Contract={sol}', f'N_Transactions={N_Transactions}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=Timeout)
 
-        if compile_process.returncode != 0:
-            raise RuntimeError(f"Compilation failed for {folder}. Error message:\n{compile_error.decode()}")
+            if compile_process.returncode != 0:
+                raise RuntimeError(f"Compilation failed for {folder}. Error message:\n{compile_process.stderr.decode()}")
 
-        run_process = subprocess.Popen(['make', 'run', 'SMT=cvc5'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        run_output, run_error = run_process.communicate()
+            # Run the code
+            run_process = subprocess.run(['make', 'run', 'SMT=cvc5'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=Timeout)
 
-        if run_process.returncode != 0:
-            raise RuntimeError(f"Execution failed for {folder}. Error message:\n{run_error.decode()}")
+            if run_process.returncode != 0:
+                raise RuntimeError(f"Execution failed for {folder}. Error message:\n{run_process.stderr.decode()}")
 
-        res = run_output.decode()
-        for phi in res.split('PROPERTY:'):
-            if 'out/' not in phi: continue
-            phi = phi.split('\n')
+            res = run_process.stdout.decode()
+            for phi in res.split('PROPERTY:'):
+                if 'out/' not in phi: continue
+                phi = phi.split('\n')
 
-            if '_nonliq' in phi[0]:
-                if 'NOT LIQUID' not in phi[-2]:
-                    os.chdir('..')
-                    pytest.fail(f'PROPERTY: {phi[0]}')
-            else:
-                if 'NOT LIQUID' in phi[-2]:
-                    os.chdir('..')
-                    pytest.fail(f'PROPERTY: {phi[0]}')
-
-        clean_process = subprocess.Popen(['make', 'clean'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        clean_output, clean_error = clean_process.communicate()
+                if '_nonliq' in phi[0]:
+                    if 'NOT LIQUID' not in phi[-2]:
+                        os.chdir('..')
+                        pytest.fail(f'PROPERTY: {phi[0]}')
+                else:
+                    if 'NOT LIQUID' in phi[-2]:
+                        os.chdir('..')
+                        pytest.fail(f'PROPERTY: {phi[0]}')
+        except:
+            pass
+        finally:
+            clean_process = subprocess.Popen(['make', 'clean'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            clean_output, clean_error = clean_process.communicate()
 
     os.chdir('..')
 
