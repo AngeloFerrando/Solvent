8f188c7ad88fd40b489035a495ac04eec1bc42b5
Massimo Bartoletti <bart@unica.it>
2024-04-16 12:12:49 +0200

    added various crowdfund versions

--- contracts/crowdfund/README.md
+++ contracts/crowdfund/README.md
@@ -1,10 +1,16 @@
 # Crowdfund
 
+## Specification
+
 The Crowdfund contract implements a crowdfunding campaign. 
 
-The constructor specifies the `owner` of the campaign, the `deadline` for receiving donations, the minimum amount for donations, and the `target` in units of token T that must be reached for the campaign to be successful. 
+The constructor specifies the `owner` of the campaign, the `deadline` for receiving donations, the minimum amount for donations, and the `target` that must be reached for the campaign to be successful. 
 
 The contract implements the following functions:
-- `deposit`, which allows anyone to deposit any amount of T in the contract. Donations are only possible before the donation period has ended;
-- `wdOwner`, which allows the `owner` to redeem all the funds deposited in the contract. This is only possible if the campaign `target` has been reached;   
-- `wdDonor`, which all allows donors to reclaim their donations after the donation period has ended.
+- `deposit`, which allows anyone to deposit any amount of tokens in the contract. Donations are only possible before the donation period has ended;
+- `wdOwner`, which allows the `owner` to redeem all the tokens deposited in the contract. This is only possible if the campaign `target` has been reached;   
+- `wdDonor`, which allows donors to reclaim their donations after the donation period has ended.
+
+## Properties
+- **owner_wd**: if threshold is reached, the owner can withdraw at least the target after the deadline
+- **donor_wd**: if threshold is not reached, donors can withdraw their donations after the deadline
---
+++ contracts/crowdfund/crowdfund-fix2.sol
@@ -0,0 +1,92 @@
+contract Crowdfund {
+    const int end_donate    // last block in which users can donate
+    const int target        // amount of ETH that must be donated for the crowdfunding to be succesful
+    const address owner     // receiver of the donated funds
+    mapping (address => int) donors
+    bool target_reached     // true when the target is reached
+    int tot_donations
+
+    constructor(address owner_, int end_donate_, int target_) {
+        owner = owner_;
+        end_donate = end_donate_;
+	      target = target_
+    }
+    
+    function donate() payable {
+        require (block.number <= end_donate);
+        donors[msg.sender] = donors[msg.sender] + msg.value;
+        tot_donations = tot_donations + msg.value;
+        if (balance >= target) { target_reached = true }
+    }
+
+    function wdOwner() {
+        require (target_reached && block.number > end_donate);
+        owner!balance
+    }
+
+    function wdRest() {
+        require (balance > tot_donations && block.number > end_donate);
+        owner!(balance-tot_donations)
+    }
+
+    function wdDonor() { 
+        require (block.number > end_donate);
+        require (not target_reached); // FIX: check target_reached
+        require (donors[msg.sender] > 0);
+        msg.sender!donors[msg.sender];
+        tot_donations = tot_donations - donors[msg.sender];
+        donors[msg.sender] = 0
+    }
+}
+
+// if target is reached, the owner can withdraw at least the target after the deadline
+property owner_wd_live {
+    Forall xa
+    [
+      st.target_reached && st.balance>=st.target && st.block.number > st.end_donate
+        ->
+      Exists tx [1, st.owner]
+      [
+        ((app_tx_st.balance[st.owner] >= st.balance[st.owner] + st.target))
+      ]
+    ]
+}
+
+// if threshold is not reached, donors can withdraw their donations after the end_donate
+property donor_wd_live {
+    Forall xa
+    [
+      st.donors[xa]>0 && (not target_reached) && st.block.number>st.end_donate
+        ->
+      Exists tx [1, xa]
+      [
+        ((app_tx_st.balance[xa] >= st.balance[xa]+st.donors[xa]))
+      ]
+    ]
+}
+
+// if threshold is reached, the owner can withdraw at least the target after the deadline
+property liquidity_live {
+    Forall xa
+    [
+      st.balance>0 && st.tot_donations==0 && st.block.number > st.end_donate
+        ->
+      Exists tx [1, xa]
+      [
+        (app_tx_st.balance==0)
+      ]
+    ]
+}
+
+// if there is some donor with enough tokens, then the campaign can be successful
+property can_reach_target_live {
+    Forall xa
+    [
+      st.balance[xa] > st.target && st.block.number <= st.end_donate
+        ->
+      Exists tx [1, xa]
+      [
+        (app_tx_st.balance >= app_tx_st.target)
+      ]
+    ]
+}
