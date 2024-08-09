81bc31c28d8803cbc1ec393c01e72d051e33b4e4
elipparini <enricolipparini@tiscali.it>
2024-08-09 18:21:18 +0200

    Reordering, cleaning pt 1

--- contracts/test_regression.py
+++ contracts/test_regression.py
@@ -2,8 +2,10 @@ import os
 import subprocess
 import time
 
-N_Transactions = 5
-Timeout = 20 # seconds
+N_Transactions = 500
+Timeout = 5 # seconds
+solver = "cvc5"
+try_statebased_iter = 5 # Try statebased after given number of iterations
 
 def run_makefile(folder):
     # Change directory to the specified folder
@@ -19,69 +21,157 @@ def run_makefile(folder):
         passed = 0
         not_passed = 0
         timeout = 0
+        starting_time_sol = time.time()
+        liquid_up_to = None
+        status = None
 
         print('\n---------------------')
         print(f'Contract: {sol}\n')
         try:
-            # Start timing
-            start_time = time.time()
-
-            # Compile the code
-            compile_process = subprocess.run(['make', 'compile', f'Contract={sol}', f'N_Transactions={N_Transactions}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=Timeout)
             
-            if compile_process.returncode != 0:
-                print(f"Compilation failed for {folder}. Error message:\n{compile_process.stderr.decode()}")
-                return
+            compilation_time = 0
+            running_time = 0
+            compile_and_run_time = 0
+            for iteration in range(1, N_Transactions+1):
+                # Start timing
+                start_time = time.time()
+                """
+                # Compile the code
+                #compile_process = subprocess.Popen(['make', 'compile', f'Contract={sol}', f'N_Transactions={N_Transactions}', 'Can_Transactions_Arrive_Any_time=True', f'Fixed_Iteration={iteration}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                #compile_run = compile_process.communicate(compile_process, timeout=Timeout)
+                
 
-            end_time = time.time()
-            compilation_time = end_time - start_time
+                compile_process = subprocess.run(['make', 'compile', f'Contract={sol}', f'N_Transactions={N_Transactions}', 'Can_Transactions_Arrive_Any_time=True', f'Fixed_Iteration={iteration}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=Timeout)
+                
+                if compile_process.returncode != 0:
+                    print(f"Compilation failed for {folder}. Error message:\n{compile_process.stderr.decode()}")
+                    return
 
-            # Start timing
-            start_time = time.time()
+                end_time = time.time()
+                compilation_time += end_time - start_time
 
-            # Run the code
-            run_process = subprocess.run(['make', 'run', 'SMT=cvc5'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=Timeout)
-            
-            if run_process.returncode != 0:
-                print(f"Execution failed for {folder}. Error message:\n{run_process.stderr.decode()}")
-                return
+                # Start timing
+                start_time = time.time()
 
-            end_time = time.time()
-            running_time = end_time - start_time
+                # Run the code
+                run_process = subprocess.run(['make', 'run', 'SMT=cvc5'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=Timeout)
+                """
+                # Start timing
+                start_time = time.time()
 
-            # Print the output of the make run command
-            # print(f"Output for {folder}:\n")
-            res = run_process.stdout.decode()
-            for phi in res.split('PROPERTY:'):
-                if 'out/' not in phi: continue
-                phi = phi.split('\n')
-                print(f'PROPERTY: {phi[0]}')
-                if '_nonliquid' in phi[0] or '_notliquid' in phi[0]:
-                    if 'NOT LIQUID' in phi[-2]:
-                        print_passed()
-                        passed += 1
-                    elif 'LIQUID'  in phi[-2]:
-                        print_not_passed()
-                        not_passed += 1
-                    else:
-                        print_not_passed()
-                        not_passed += 1
+                if iteration == try_statebased_iter:  # Try statebased
+                    compile_and_run_process = subprocess.run(f"make compile Contract={sol} N_Transactions={N_Transactions} Can_Transactions_Arrive_Any_time=True Fixed_Iteration={iteration}; echo end_compile_start_run; make run SMT={solver}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=Timeout)
                 else:
-                    if 'NOT LIQUID'  in phi[-2]:
-                        print_not_passed()
-                        not_passed += 1
-                    elif 'LIQUID' in phi[-2]:
-                        print_passed()
-                        passed += 1
-                    else:
-                        print_not_passed()
-                        not_passed += 1
-                print('')
+                    compile_and_run_process = subprocess.run(f"make compile Contract={sol} N_Transactions={N_Transactions} Can_Transactions_Arrive_Any_time=True Fixed_Iteration={iteration} State_Based=false; echo end_compile_start_run; make run SMT={solver} State_Based=false", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=Timeout)
 
-            print(f"Compilation time: {compilation_time} seconds; Running time: {running_time} seconds")
+
+                if compile_and_run_process.returncode != 0:
+                    print(f"Compilation or Execution failed for {folder}. Error message:\n{compile_and_run_process.stderr.decode()}")
+                    return
+
+                end_time = time.time()
+                compile_and_run_time += end_time - start_time
+
+                # If timeout passed, don't consider last result
+                if compile_and_run_time > Timeout:
+                    compile_and_run_time = Timeout     # TODO
+                    break
+
+
+                # Print the output of the make run command
+                # print(f"Output for {folder}:\n")
+                stop = False
+                ok = False
+                res = compile_and_run_process.stdout.decode()
+                #print(f"{res=}")
+                res_compile, res_run = res.split("end_compile_start_run")
+                for phi in res_run.split('PROPERTY:'):
+                    if 'out/' not in phi: continue
+                    phi = phi.split('\n')
+                    if iteration == 1: print(f'PROPERTY: {phi[0]}')
+                    if '_nonliquid' in phi[0] or '_notliquid' in phi[0]:
+                        if 'NOT LIQUID' in phi[-2]:
+                            print_passed()
+                            print_not_liquid(iteration)
+                            print('')
+                            passed += 1
+                            status = "passed"
+                            stop = True
+                            ok = True
+                        elif 'LIQUID'  in phi[-2]  and not 'UP TO' in phi[-2]:        # Strong unsat
+                            print_not_passed()
+                            not_passed += 1
+                            status = "not passed"
+                            stop = True
+                            pass
+                        elif 'LIQUID'  in phi[-2]  and 'UP TO' in phi[-2]:    # Weak unsat
+                            pass
+                            # print_not_passed()
+                            # not_passed += 1
+                        else:
+                            pass
+                            # print_not_passed()
+                            # not_passed += 1
+                    else:
+                        #print(f"{phi[-2]=}")
+                        if 'NOT LIQUID'  in phi[-2]:
+                            ok = False
+                            stop = True
+                            # print_not_passed()
+                            # not_passed += 1
+                        elif 'LIQUID' in phi[-2] and 'UP TO' not in phi[-2]:  # Strong unsat
+                            stop = True
+                            ok = True
+                            print_passed()
+                            print_liquid(-1)
+                            print('')
+                            status = "passed"
+                            passed += 1
+                        elif 'LIQUID' in phi[-2]:
+                            ok = True
+                            liquid_up_to = iteration
+                            #print(f"{liquid_up_to=}")
+                            # print_not_passed()
+                            # not_passed += 1
+                        else:
+                            ok = True
+                            # print_not_passed()
+                            # not_passed += 1
+                if stop:
+                    break
+            if not ok:
+                print_not_passed()
+                print('')
+                status = "not passed"
+                not_passed += 1
+            elif not stop and ok:
+                print_passed()
+                print_liquid(iteration)
+                print('')
+                status = "passed"
+                passed += 1
+            
+            print(f"Time: {compile_and_run_time} seconds")
+            #print(f"Compilation time: {compilation_time} seconds; Running time: {running_time} seconds")
         except subprocess.TimeoutExpired:
-            timeout += 1
-            print(f"Timeout for {sol}")
+            pass 
+            #timeout += 1
+            #print(f"Timeout for {sol}")
+        if not status:
+            #print(f"{liquid_up_to=}")
+            if liquid_up_to:
+                print_passed()
+                print_liquid(liquid_up_to)
+                print('')
+                print(f"Time: {compile_and_run_time} seconds")
+                status = "passed"
+                passed += 1
+            else:
+                status = "timeout"  
+                timeout += 1
+                print(f"Timeout for {sol}")
+
+
         clean_process = subprocess.Popen(['make', 'clean'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         clean_output, clean_error = clean_process.communicate()
         dict_res[sol] = (passed, not_passed, timeout)
@@ -96,9 +186,24 @@ def print_passed():
 def print_not_passed():
     print("\033[91mNot Passed\033[0m", end='', flush=True)  # ANSI escape code for red text
 
+def print_not_liquid(i):
+    if i == 1:
+        print(f"\t - \t\033[94mNOT LIQUID (counterexample found in {i} step)\033[0m\n", end='', flush=True)  # ANSI escape code for blue text
+    else:
+        print(f"\t - \t\033[94mNOT LIQUID (counterexample found in {i} steps)\033[0m\n", end='', flush=True)  # ANSI escape code for blue text
 
-# List all directories in the current directory
-directories = ['regression'] #sorted([d for d in os.listdir('.') if os.path.isdir(d)])
+
+def print_liquid(i = None):
+    if i == -1:         # LIQUID
+        print(f"\t - \t\033[93mLIQUID\033[0m\n", end='', flush=True)  # ANSI escape code for blue text
+    elif i >= 0:        # LIQUID up to i
+        print(f"\t - \t\033[93mLIQUID (up to {i-1})\033[0m\n", end='', flush=True)  # ANSI escape code for blue text
+    # else -> unknown, don't print anything
+
+
+# List regression directory
+directories = ['regression'] 
+
 
 dict_res = {}
 # Run makefile for each directory
