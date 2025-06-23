c7f9b63e7d4fb30c1697a42487df270e98032175
elipparini <enricolipparini@protonmail.com>
2025-06-23 09:54:02 +0200

    +prop new logic Blacklist

--- contracts/regression/blacklist.sol
+++ contracts/regression/blacklist.sol
@@ -12,6 +12,25 @@ contract Blacklist {
 
 }
 
+
+rule P1_nonliquid {
+  forall a : address .
+  exists v : int .   
+    << a : Blacklist . pay() $ v >>		
+      balance[a] == old(balance[a]) + balance
+}
+
+/*
+rule P2_liquid {
+  forall a : address .
+  a!=1 ->
+  exists v : int .   
+    << a : Blacklist . pay() $ v >>		
+      balance[a] == old(balance[a]) + balance
+}
+*/
+
+/*
 // STRONG SAT (Can_Transactions_Arrive_Any_time=False WEAK SAT WEAK UNSAT)
 property liquidity1_nonliquid {
     Forall xa
@@ -37,3 +56,4 @@ property liquidity2_liquid {
       ]
     ]
 }
+*/
