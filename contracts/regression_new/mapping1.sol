ea971f4a4591267ee4f446ce2f3e9bcdff424670
elipparini <enricolipparini@protonmail.com>
2025-07-09 15:53:04 +0200

    +mapping1

---
+++ contracts/regression_new/mapping1.sol
@@ -0,0 +1,32 @@
+contract Mapping1 {
+    mapping (address => int) m;
+
+    function foo() {
+        m[msg.sender] = m[msg.sender] + 1
+    }
+}  
+
+rule P1_true {
+    forall a : address .
+    exists h : method .
+        << a : Mapping1 . h() $ 0 >> 
+            m[a] == old(m[a]) + 1
+}
+
+
+rule P2_false {
+    forall a : address .
+    exists h : method .
+        << a : Mapping1 . h() $ 0 >> 
+            m[a] == old(m[a]) + 2
+}
+
+
+rule P3_true {
+    forall a : address .
+    exists h1 : method .
+    exists h2 : method .
+        << a : Mapping1 . h1() $ 0 >> 
+          << a : Mapping1 . h2() $ 0 >> 
+              m[a] == old(old(m[a])) + 2
+}
