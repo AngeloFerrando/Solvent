a1b35d56b14acb3b1081baba9280821314e5c888
elipparini <enricolipparini@protonmail.com>
2025-07-22 18:40:49 +0200

    update amm.sol

--- contracts/amm/amm.sol
+++ contracts/amm/amm.sol
@@ -1,6 +1,5 @@
 
 contract AMM {
-    address immutable a0;
 	mapping (address => int) bal0;
 	mapping (address => int) bal1;
     bool tout;
@@ -19,15 +18,19 @@ contract AMM {
 		require(xin > 0);
 		tout = not tin;
 		if (tout) {
-            yout = xin * (bal1[a0] / bal0[a0]);
-			require (yout >= ymin && yout<bal1[a0]);
-			bal1[a0] = bal1[a0] - yout;
+            yout = xin * (bal1[this] / bal0[this]);
+			require (yout >= ymin && yout<bal1[this]);
+			bal0[this] = bal0[this] + xin;
+			bal0[msg.sender] = bal0[msg.sender] - xin;
+			bal1[this] = bal1[this] - yout;
 			bal1[msg.sender] = bal1[msg.sender] + yout
         }
 		else {
-            yout = xin * bal0[a0] / bal1[a0];
-			require (yout >= ymin && yout<bal0[a0]);
-			bal0[a0] = bal0[a0] - yout;
+            yout = xin * bal0[this] / bal1[this];
+			require (yout >= ymin && yout<bal0[this]);
+			bal1[this] = bal1[this] + xin;
+			bal1[msg.sender] = bal1[msg.sender] - xin;
+			bal0[this] = bal0[this] - yout;
 			bal0[msg.sender] = bal0[msg.sender] + yout
         }
 	}
