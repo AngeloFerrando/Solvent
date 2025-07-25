10f93865675052ce6819ec2d6b5c4e18c9d3caf3
elipparini <enricolipparini@protonmail.com>
2025-07-25 20:31:44 +0200

    update amm properties

--- contracts/amm/amm.sol
+++ contracts/amm/amm.sol
@@ -18,33 +18,56 @@ contract AMM {
 		require(xin > 0);
 		tout = not tin;
 		if (tout) {
-            yout = xin * (bal1[this] / bal0[this]);
-			require (yout >= ymin && yout<bal1[this]);
-			bal0[this] = bal0[this] + xin;
-			bal0[msg.sender] = bal0[msg.sender] - xin;
-			bal1[this] = bal1[this] - yout;
-			bal1[msg.sender] = bal1[msg.sender] + yout
-        }
+        yout = xin * (bal1[this] / bal0[this]);
+        require (yout >= ymin && yout<bal1[this]);
+        bal0[this] = bal0[this] + xin;
+        bal0[msg.sender] = bal0[msg.sender] - xin;
+        bal1[this] = bal1[this] - yout;
+        bal1[msg.sender] = bal1[msg.sender] + yout
+      }
 		else {
-            yout = xin * bal0[this] / bal1[this];
-			require (yout >= ymin && yout<bal0[this]);
-			bal1[this] = bal1[this] + xin;
-			bal1[msg.sender] = bal1[msg.sender] - xin;
-			bal0[this] = bal0[this] - yout;
-			bal0[msg.sender] = bal0[msg.sender] + yout
-        }
+        yout = xin * bal0[this] / bal1[this];
+        require (yout >= ymin && yout<bal0[this]);
+        bal1[this] = bal1[this] + xin;
+        bal1[msg.sender] = bal1[msg.sender] - xin;
+        bal0[this] = bal0[this] - yout;
+        bal0[msg.sender] = bal0[msg.sender] + yout
+      }
 	}
 }
 
 
-
-rule Dummy1 {
-    True
+rule Test {
+  forall a : address .  
+    << a : AMM . swap(0 , False , 0) $ 0 >>		
+      true
 }
 
 /*
-rule Dummy {
+
+rule P1_false {
   forall a : address .  
     << a : AMM . swap(0 , False , 0) $ 0 >>		
-      bal0[a] == old(bal0[a]) + 1
-}*/
+      bal0[this] > old(bal0[this])
+}
+
+rule P2_true {
+  forall a : address .  
+    << a : AMM . swap(1 , False , 0) $ 0 >>		
+      bal0[this] > old(bal0[this])
+}
+
+
+rule P3_false {
+  forall a : address .  
+    << a : AMM . swap(0 , False , 0) $ 0 >>		
+      bal1[this] < old(bal1[this])
+}
+
+rule P4_true {
+  forall a : address .  
+    << a : AMM . swap(1 , False , 0) $ 0 >>		
+      bal1[this] < old(bal1[this])
+}
+
+*/
