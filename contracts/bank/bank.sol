d147451d111ad1c0d4fad8a315ee66f95b54d8e1
elipparini <enricolipparini@protonmail.com>
2026-01-12 19:45:20 +0100

    bank.sol: update

--- contracts/bank/bank.sol
+++ contracts/bank/bank.sol
@@ -20,27 +20,68 @@ contract Bank {
     }
 }  
 
-property deposit_not_revert_liquid {
-    Forall xa
-      [
-        balance[xa]>0 
-          -> 
-        Exists tx [1, xa]
-        [
-          (<tx>funds[xa] == funds[xa] + balance[xa])
-        ]
-      ]
-}
+// rule Additivity {
+//     forall c1 : int .
+//     forall c2 : int .
+//     exists c3 : int .
+//     exists v12_storage : int .
+//     exists v3_storage : int .
+//     exists v : int .
+//     forall addr : address .
+//     (<< addr : Bank . deposit() $ c1 >>		
+//        (<< addr : Bank . deposit() $ c2 >>		
+//             (v12_storage == v)))
+//     &&
+//     (<< addr : Bank . deposit() $ c3 >>		
+//             (v3_storage == v))
+//     &&
+//     (v12_storage == v3_storage)
+// }
 
 
-property withdraw_not_revert_liquid {
-    Forall xa
-      [
-        funds[xa]>0 
-          -> 
-        Exists tx [1, xa]
-        [
-          (<tx>balance[xa] >= balance[xa] + funds[xa])
-        ]
-      ]
+// rule Reversibility_deposit {
+//     forall addr : address .
+//     forall c1 : int .
+//     exists f: method .
+//     exists args: calldataargs .
+//     exists c2 : int .
+//     (<< addr : Bank . deposit() $ c1 >>		
+//        (<< addr : Bank . f(args) $ c2 >>		
+//             (funds[addr] == old(old(funds[addr])))))
+// }
+
+
+rule Liquidity {
+    forall addr : address .
+    exists f: method .
+    exists args: calldataargs .
+    exists c1 : int .
+    (<< addr : Bank . f(args) $ c1 >>		
+            funds[addr] == 0
+    )
 }
+
+// property deposit_not_revert_liquid {
+//     Forall xa
+//       [
+//         balance[xa]>0 
+//           -> 
+//         Exists tx [1, xa]
+//         [
+//           (<tx>funds[xa] == funds[xa] + balance[xa])
+//         ]
+//       ]
+// }
+
+
+// property withdraw_not_revert_liquid {
+//     Forall xa
+//       [
+//         funds[xa]>0 
+//           -> 
+//         Exists tx [1, xa]
+//         [
+//           (<tx>balance[xa] >= balance[xa] + funds[xa])
+//         ]
+//       ]
+// }
