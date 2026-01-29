9065d940b0d79c36b005f0869d826d7843f30a41
elipparini <enricolipparini@protonmail.com>
2026-01-29 17:23:24 +0100

    amm_nothis.sol: update

--- contracts/amm/amm_nothis.sol
+++ contracts/amm/amm_nothis.sol
@@ -1,27 +1,30 @@
 
 contract AMM {
+  int counter;
 	mapping (address => int) bal0;
 	mapping (address => int) bal1;
-  int bal0_AMM;
-  int bal1_AMM;
+  uint bal0_AMM;
+  uint bal1_AMM;
     bool tout;
-    int yout;
+    uint yout;
 
-	constructor(int x0, int x1) {
+	constructor(uint x0, uint x1) {
         require(x0 > 0 && x1 > 0);
         //bal0[msg.sender] = bal0[msg.sender] - x0;
         //bal1[msg.sender] = bal1[msg.sender] - x1;  
         bal0[msg.sender] = 100;
         bal1[msg.sender] = 50;  
         bal0_AMM = x0;
-        bal1_AMM = x1
+        bal1_AMM = x1;
+        counter = 0
     }
 
     /* tin is 0 if T0; 1 if T1 */
-	function swap(int xin, bool tin, int ymin) {
+	function swap(uint xin, bool tin, uint ymin) {
     require(bal0_AMM > 0 && bal1_AMM > 0);
 		require(xin > 0);
 		tout = not tin;
+    counter = counter + 1;
 		if (tout) {
         yout = xin * (bal1_AMM / bal0_AMM);
         require (yout >= ymin && yout<bal1_AMM);
@@ -62,21 +65,22 @@ contract AMM {
 // }
 
 
-// // if swap reverts than bal0_AMM does not increase
+// if swap reverts than bal0_AMM does not increase
 // rule P2_false { 
 //   forall a : address .  
 //     << a : AMM . swap(1 , False , 0) $ 0 >>		
 //       bal0_AMM > old(bal0_AMM)
 // }
 
-// // with Ints, we can have yout = 0 due to approximations
-// rule P2b_false {
+// if not revert, than bal0_AMM always increases of xin
+// rule P2b_true {
 //   forall a : address .  
 //     << a : AMM . swap(1 , False , 0) $ 0 >>		
 //     !lastReverted ->
 //       bal0_AMM > old(bal0_AMM)
 // }
 
+// rule è sempre possibile fare un gain (falso con Int, vero con i Real; assumiamo che i prezzi siano 1 e che l'AMM sia balanced, ovvero stesso numero di token)
 
 
 // rule P3_false {
@@ -101,11 +105,33 @@ contract AMM {
 //       bal1_AMM < old(bal1_AMM)
 // }
 
-rule P4c_true {
-  forall a : address .  
-    << a : AMM . swap(1 , False , 1) $ 0 >>		
-      !lastReverted 
-        ->
-      bal1_AMM < old(bal1_AMM)
-}
+// rule P4c_true {
+//   forall a : address .  
+//     << a : AMM . swap(1 , False , 1) $ 0 >>		
+//       !lastReverted 
+//         ->
+//       bal1_AMM < old(bal1_AMM)
+// }
 
+
+// False with Ints
+// True with Reals
+// rule Constant_product {
+//     counter > 0 ->
+//       forall a : address .  //redundant
+//       forall args: calldataargs .
+//       forall f: method .
+//       forall msgvalue: int .
+//         << a : AMM . f(args) $ msgvalue >>		
+//           bal0_AMM*bal1_AMM == old(bal0_AMM*bal1_AMM)
+// }
+
+rule Constant_product_violation {
+    counter > 0 ->
+      exists a : address .  //redundant
+      exists args: calldataargs .
+      exists f: method .
+      exists msgvalue: int .
+        << a : AMM . f(args) $ msgvalue >>		
+          bal0_AMM*bal1_AMM != old(bal0_AMM*bal1_AMM)
+}
